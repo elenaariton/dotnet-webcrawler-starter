eac85c58d5edc6e0ebe1749f0fcfe7d60477c0e1
Elena Ariton <eariton@veinteractive.com>
2020-05-23 10:52:00 +0300

    Adding first tests.

---
+++ Business/Implementation/HtmlParser.cs
@@ -0,0 +1,13 @@
+﻿using Buildit.Webcrawler.Business.Interfaces;
+using System.Collections.Generic;
+
+namespace Buildit.Webcrawler.Business.Implementation
+{
+    public class HtmlParser : IHtmlParser
+    {
+        public List<string> GetAllLinks(string testPageHtml)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
